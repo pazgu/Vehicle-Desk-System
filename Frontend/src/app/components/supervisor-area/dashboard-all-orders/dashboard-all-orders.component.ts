1dcf914da0d1195795592f21178f1470bc95c3c4
leen <leen051206@gmail.com>
2025-05-07 11:36:02 +0300

    added dashboard all orders components

---
+++ Frontend/src/app/components/supervisor-area/dashboard-all-orders/dashboard-all-orders.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-all-orders',
+  imports: [],
+  templateUrl: './dashboard-all-orders.component.html',
+  styleUrl: './dashboard-all-orders.component.css'
+})
+export class DashboardAllOrdersComponent {
+
+}
