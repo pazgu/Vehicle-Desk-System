3ef5cbc5863cd9ddc0d42dfa4c552b1326738426
sarahmanso <sarahmansor2580@gmail.com>
2025-05-11 11:39:48 +0300

    cleaner code

--- Backend/src/models/user_model.py
+++ Backend/src/models/user_model.py
@@ -10,8 +10,8 @@ class UserRole(str, enum.Enum):
     admin = 'admin'
 
 class User(Base):
-    __tablename__ = 'users'
-
+    __tablename__ = 'users'  
+    
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
