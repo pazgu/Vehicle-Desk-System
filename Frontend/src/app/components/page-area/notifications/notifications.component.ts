ae9c4819697f3bb305faf61148402ee390dbe0a6
guev06 <jojamisto@gmail.com>
2025-05-13 11:11:04 +0300

    fixed all paginations

--- Frontend/src/app/components/page-area/notifications/notifications.component.ts
+++ Frontend/src/app/components/page-area/notifications/notifications.component.ts
@@ -13,11 +13,13 @@ export class NotificationsComponent {
     { timeAgo: 'לפני 30 דק׳', message: 'בקשתך לנסיעה לתאריך 31.07.2025 אושרה', status: 'Approved' },
     { timeAgo: 'לפני שעה', message: 'בקשתך לנסיעה בתאריך 02.6.2025 נדחתה', status: 'Rejected' },
     { timeAgo: 'לפני 3 שע׳', message: 'בקשתך לנסיעה בתאריך 23.7.2025 נדחתה', status: 'Rejected' },
+    { timeAgo: 'אתמול, 1:59', message: 'בקשתך לנסיעה בתאריך 5.7.2025 אושרה', status: 'Approved' },
+    { timeAgo: 'לפני 3 שע׳', message: 'בקשתך לנסיעה בתאריך 23.7.2025 נדחתה', status: 'Rejected' },
     { timeAgo: 'אתמול, 1:59', message: 'בקשתך לנסיעה בתאריך 5.7.2025 אושרה', status: 'Approved' }
   ];
 
   currentPage = 1;
-  notificationsPerPage = 3;
+  notificationsPerPage = 5;
 
   get totalPages(): number {
     return Math.ceil(this.notifications.length / this.notificationsPerPage);
