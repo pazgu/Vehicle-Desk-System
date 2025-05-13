ca98d650b00a743ede41604e130589d18f062225
maysam-1 <maysamkh44@gmail.com>
2025-05-13 11:06:30 +0300

    removed fullname

--- Frontend/src/app/components/layout-area/header/header.component.ts
+++ Frontend/src/app/components/layout-area/header/header.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { Router, RouterLinkActive, RouterModule } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Observable, observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 constructor(private authService: AuthService, private router: Router) {}
-
 onLogout(): void {
   this.authService.logout();
   this.router.navigate(['/login']); 
