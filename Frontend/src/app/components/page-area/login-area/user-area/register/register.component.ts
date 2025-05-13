cf0da53eb1ae5125925604bb2ac600992d63c4e6
guev06 <jojamisto@gmail.com>
2025-05-13 08:31:34 +0300

    Fixed login and signup with toast

--- Frontend/src/app/components/page-area/login-area/user-area/register/register.component.ts
+++ Frontend/src/app/components/page-area/login-area/user-area/register/register.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../../../../services/auth.service';
+import { ToastService } from '../../../../../services/toast.service';
 
 @Component({
   selector: 'app-register',
