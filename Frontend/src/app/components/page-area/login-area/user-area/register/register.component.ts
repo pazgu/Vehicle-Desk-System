f4e741cf2cd5be3b5564f1af7184efefa999da76
Karen7-salih <Karen7-salih.salihkaren345@gmail.com>
2025-05-13 15:25:28 +0300

    added the local storage aka employee id

--- Frontend/src/app/components/page-area/login-area/user-area/register/register.component.ts
+++ Frontend/src/app/components/page-area/login-area/user-area/register/register.component.ts
@@ -84,6 +84,8 @@ export class RegisterComponent implements OnInit {
         localStorage.setItem('first_name', response.first_name);
         localStorage.setItem('last_name', response.last_name);
         localStorage.setItem('role', response.role);
+        localStorage.setItem('employee_id', response.employee_id);
+
 
         this.authService.setFullName(response.first_name, response.last_name);
         this.authService.setLoginState(true);
