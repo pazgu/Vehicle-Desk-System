118d4ad471e8c4a39c98557268efd6e739549096
guev06 <jojamisto@gmail.com>
2025-05-13 15:58:45 +0300

    lalala

--- Frontend/src/app/services/redirect-by-role.ts
+++ Frontend/src/app/services/redirect-by-role.ts
@@ -12,7 +12,10 @@ export class RedirectByRoleComponent implements OnInit {
   ngOnInit(): void {
     const role = localStorage.getItem('role');
 
-    if (role === 'admin') {
+    if (!role) {
+      // No role found in localStorage, redirect to login page
+      this.router.navigate(['/login']);
+    } else if (role === 'admin') {
       this.router.navigate(['/daily-checks']);
     } else if (role === 'supervisor') {
       this.router.navigate(['/supervisor-dashboard']);
