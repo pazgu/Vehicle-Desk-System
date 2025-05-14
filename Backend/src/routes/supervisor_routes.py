a8094ff2257bf5c081482a6d84206fa537f31d95
sarahmanso <sarahmansor2580@gmail.com>
2025-05-14 11:28:10 +0300

    added patch to supervisor update order

--- Backend/src/routes/supervisor_routes.py
+++ Backend/src/routes/supervisor_routes.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from uuid import UUID
 from src.services.supervisor_dashboard_service import get_department_orders
-
+from sqlalchemy.orm import Session
+from src.schemas.ride_status_enum import UpdateRideStatusRequest
+from src.services.user_rides_service import update_ride_status
+from src.utils.database import get_db
 
 
 router = APIRouter()
@@ -29,3 +32,12 @@ def get_department_vehicles(department_id: UUID):
 @router.get("/notifications/{department_id}")
 def view_department_notifications(department_id: UUID):
     return {"message": f"Notifications for department {department_id}"}
+
+@router.patch("/orders/{department_id}/{ride_id}/update")
+def supervisor_update_ride_status(
+    department_id: UUID,
+    ride_id: UUID,
+    req: UpdateRideStatusRequest,
+    db: Session = Depends(get_db)
+):
+    return update_ride_status(ride_id, req.status, db)
