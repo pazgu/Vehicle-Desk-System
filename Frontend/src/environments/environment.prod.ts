17ed26a0f1ed914acac8d65fd297b616c678eac2
Karen7-salih <Karen7-salih.salihkaren345@gmail.com>
2025-05-14 12:10:11 +0300

    connected my orders to backend

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -3,6 +3,10 @@ export const environment = {
     loginUrl: 'http://localhost:8000/api/login' ,
     registerUrl: 'http://localhost:8000/api/register',
     notificatioUrl:'http://localhost:8000/api/notifications',
+    allOrdersUrl: 'http://localhost:8000/api/all-orders',
+    futureOrdersUrl: 'http://localhost:8000/api/future-orders',
+    pastOrdersUrl: 'http://localhost:8000/api/past-orders',
+
 
 
 
