9a5690e5d02ca212d8690f23428686e8977fb07b
maysam-1 <maysamkh44@gmail.com>
2025-05-14 22:11:05 +0300

    fixed get orders feature

--- Backend/src/services/user_rides_service.py
+++ Backend/src/services/user_rides_service.py
@@ -6,6 +6,8 @@ from ..schemas.user_rides_schema import RideSchema
 from uuid import UUID
 from ..models.vehicle_model import Vehicle
 from sqlalchemy import String
+from datetime import datetime, timezone
+
 
 def filter_rides(query, status: Optional[RideStatus], from_date, to_date):
     if status:
@@ -22,32 +24,34 @@ def filter_rides(query, status: Optional[RideStatus], from_date, to_date):
 
 
 def get_future_rides(user_id: UUID, db: Session, status=None, from_date=None, to_date=None) -> List[RideSchema]:
-    # Updated query with join and casting
-    now = datetime.now()  # naive datetime for naive DB
+    # Use naive datetime for Postgres 'timestamp without time zone'
+    mynow = datetime.utcnow().replace(tzinfo=None)
 
     query = db.query(
         Ride.id.label("ride_id"),
         Vehicle.fuel_type.label("vehicle"),
         Ride.start_datetime,
         Ride.end_datetime,
-        Ride.estimated_distance_km.cast(String).label("estimated_distance"),  # Casting to string
+        Ride.estimated_distance_km.cast(String).label("estimated_distance"),
         Ride.status
-    ).join(Vehicle, Ride.vehicle_id == Vehicle.id).filter(Ride.user_id == user_id, Ride.start_datetime > now)
+    ).join(Vehicle, Ride.vehicle_id == Vehicle.id).filter(
+        Ride.user_id == user_id,
+        Ride.start_datetime > mynow
+    )
 
     # Apply additional filters if necessary
     query = filter_rides(query, status, from_date, to_date)
 
-    # Fetch and return the result as a list of RideSchema
-    return query.all()
-
-
+    rows = query.all()
+    return [RideSchema(**dict(row._mapping)) for row in rows]
 
 # def get_past_rides(user_id: UUID, db: Session, status=None, from_date=None, to_date=None) -> List[Ride]:
 #     query = db.query(Ride).filter(Ride.user_id == user_id, Ride.start_datetime <= datetime.utcnow())
 #     return filter_rides(query, status, from_date, to_date).all()
 
 def get_past_rides(user_id: UUID, db: Session, status=None, from_date=None, to_date=None) -> List[RideSchema]:
-    now = datetime.now()  # naive datetime for naive DB
+    now = datetime.now(timezone.utc)
+ # naive datetime for naive DB
 
     query = db.query(
         Ride.id.label("ride_id"),
