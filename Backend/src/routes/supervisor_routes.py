98ee7ae4edb45e4a833b3a45e906284857afc9cf
yasminahalabi <yasminahalabi77@gmail.com>
2025-05-18 09:11:16 +0300

    added routes

--- Backend/src/routes/supervisor_routes.py
+++ Backend/src/routes/supervisor_routes.py
@@ -1,10 +1,14 @@
 from uuid import UUID
 from src.services.supervisor_dashboard_service import get_department_orders,get_department_specific_order,edit_order_status
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends ,  Query
 from sqlalchemy.orm import Session
 from src.utils.database import get_db
 from src.models.ride_model import Ride 
-
+from ..schemas.vehicle_schema import VehicleOut , InUseVehicleOut
+from ..models.vehicle_model import VehicleType
+from ..services.vehicle_service import get_vehicles_with_optional_status
+# get_available_vehicles as fetch_available_vehicles, get_in_use_vehicles, get_frozen_vehicles , get_vehicles_with_optional_status
+from typing import List, Optional, Union
 
 
 router = APIRouter()
@@ -38,3 +42,31 @@ def get_department_vehicles_route(department_id: UUID):
 def view_department_notifications_route(department_id: UUID):
     return {"message": f"Notifications for department {department_id}"}
 
+@router.get("/available-vehicles", response_model=List[VehicleOut])
+def available_vehicles(
+    type: Optional[VehicleType] = Query(None),
+    db: Session = Depends(get_db)
+):
+    return fetch_available_vehicles(db=db, type=type)
+
+@router.get("/in-use-vehicles", response_model=List[InUseVehicleOut])
+def in_use_vehicles( db: Session = Depends(get_db)):
+    return get_in_use_vehicles(db=db)
+
+@router.get("/frozen-vehicles", response_model=List[VehicleOut])
+def frozen_vehicles(
+    type: Optional[VehicleType] = Query(None),
+    db: Session = Depends(get_db)
+):
+    return get_frozen_vehicles(db=db, type=type)
+
+@router.get("/all-vehicles")
+def read_vehicles(status: Optional[str] = Query(None), db: Session = Depends(get_db)):
+    vehicles = get_vehicles_with_optional_status(db, status)
+
+    if status == "in_use":
+        validated = [InUseVehicleOut(**v) if isinstance(v, dict) else v for v in vehicles]
+    else:
+        validated = [VehicleOut(**v) if isinstance(v, dict) else v for v in vehicles]
+
+    return validated
