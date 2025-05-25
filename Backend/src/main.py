668e62aa666686c7872d714670bdf93da81be7a5
Karen7-salih <Karen7-salih.salihkaren345@gmail.com>
2025-05-25 11:00:26 +0300

    added import for erquest

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -3,6 +3,8 @@ from src.routes.user_routes import router as user_route
 from src.routes.supervisor_routes import router as supervisor_route
 from src.routes.admin_routes import router as admin_route
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Request
+
 
 app = FastAPI()
 
