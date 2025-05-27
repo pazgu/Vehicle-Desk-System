071e70a6746261a4b651419a20e5b6d032e4ccb8
leen <leen051206@gmail.com>
2025-05-27 09:01:19 +0300

    added service for audit logs in the frontend

--- Frontend/src/app/services/audit-logs.service.ts
+++ Frontend/src/app/services/audit-logs.service.ts
@@ -1,13 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuditLogs } from '../models/audit-logs/audit-logs.module';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuditLogsService {
+  private apiUrl = environment.apiUrl; 
+  
 
   constructor(private http: HttpClient) { }
 
-    getAuditLogs(): Observable<>
+  getAuditLogs(): Observable<AuditLogs[]>{
+    const url = `${this.apiUrl}/audit-logs`;
+    return this.http.get<AuditLogs[]>(url);  
+  }
 }
