4e3ac58efd8205af879e19de4e40127eb609cb0d
leen <leen051206@gmail.com>
2025-05-27 09:58:28 +0300

    added module for audit log in the frontend

--- Frontend/src/app/models/audit-logs/audit-logs.module.ts
+++ Frontend/src/app/models/audit-logs/audit-logs.module.ts
@@ -1,4 +1,10 @@
 export interface AuditLogs {
-    
+    id: number;
+    action: string;
+    entity_type: string;
+    entity_id: string;
+    change_data: JSON;
+    created_at: string; 
+    changed_by: string;
   }
   
