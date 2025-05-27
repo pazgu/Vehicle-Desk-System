d62d279b3ba05c211de3461624ccc312db1ab931
leen <leen051206@gmail.com>
2025-05-27 14:08:20 +0300

    frontend displays the data in the table

--- Frontend/src/app/services/audit-logs.service.ts
+++ Frontend/src/app/services/audit-logs.service.ts
@@ -1,20 +1,21 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuditLogs } from '../models/audit-logs/audit-logs.module';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuditLogsService {
-  private apiUrl = environment.apiUrl; 
-  
+  private apiUrl = 'http://localhost:8000/api/all-audit-logs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getAuditLogs(): Observable<AuditLogs[]>{
-    const url = `${this.apiUrl}/audit-logs`;
-    return this.http.get<AuditLogs[]>(url);  
+  getAuditLogs(): Observable<AuditLogs[]> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+    return this.http.get<AuditLogs[]>(this.apiUrl, { headers });
   }
 }
