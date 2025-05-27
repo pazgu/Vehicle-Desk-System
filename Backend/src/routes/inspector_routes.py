cdbef83ca2e87bef2546d165a24e9bc42de3d796
YareenHalaby <yareenh2006@gmail.com>
2025-05-27 08:49:10 +0300

    fixed an error

--- Backend/src/routes/inspector_routes.py
+++ Backend/src/routes/inspector_routes.py
@@ -7,10 +7,4 @@ from datetime import date
 
 router = APIRouter()
 
-@router.post("/")
-def create_inspection(inspection: VehicleInspectionSchema):
-    return {
-        "message": "Inspection received",
-        "data": inspection
-    }
 
