b4b6cdb4ec1e5d4dfb63971ebfe7b6bb658a16e3
Karen7-salih <Karen7-salih.salihkaren345@gmail.com>
2025-06-03 15:19:17 +0300

    added tghe chars falling

--- Frontend/src/app/components/auth-area/forgot-password/forgot-password.component.ts
+++ Frontend/src/app/components/auth-area/forgot-password/forgot-password.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -14,11 +15,23 @@ export class ForgotPasswordComponent {
   submitted = false;
   message = '';
   error = '';
+floatingChars: { char: string; left: number; delay: number; duration: number }[] = [];
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+
+  constructor(private fb: FormBuilder, private authService: AuthService,  private router: Router) {
     this.forgotForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]]
     });
+
+   const symbols = ['*', '#', '•', '@', '$', '%', '&'];
+  for (let i = 0; i < 50; i++) {
+    this.floatingChars.push({
+      char: symbols[Math.floor(Math.random() * symbols.length)],
+      left: Math.random() * 100, // 0–100vw
+      delay: Math.random() * 5, // seconds
+      duration: 5 + Math.random() * 5 // 5–10s
+    });
+  }
   }
 
 get f() {
@@ -39,4 +52,8 @@ get f() {
       error: err => this.error = err.error.detail || 'Failed to send reset email'
     });
   }
+
+  navigateBack() {
+  this.router.navigate(['/login']); // update this if your login route is different
+}
 }
