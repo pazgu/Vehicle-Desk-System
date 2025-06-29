8283a27f7cec3cffc546622bf957b0e89d5871e0
Annie <annienabwani46@gmail.com>
2025-06-29 13:54:35 +0300

    added animations module

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { authInterceptorProvider } from './interceptors/auth.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
     authInterceptorProvider  // 👈 Add this line
     // This enables DI-based interceptors
