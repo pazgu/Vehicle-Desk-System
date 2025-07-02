4151f1b886d61e462754ec3ada68944cb3b192d6
yasminahalabi <yasminahalabi77@gmail.com>
2025-07-02 10:13:44 +0300

    added upload_license_file to the frontend

--- Frontend/src/app/components/admin-area/user-card/user-card.component.ts
+++ Frontend/src/app/components/admin-area/user-card/user-card.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 import { User } from '../../../models/user.model';
 import { UserService } from '../../../services/user_service';
+import { environment } from '../../../../environments/environment';
+
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.css'],
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
 })
 export class UserCardComponent implements OnInit {
   user: User | null = null;
   userId: string | null = null;
+  public baseUrl = environment.socketUrl;
+
 
   departmentNames: { [key: string]: string } = {
     '21fed496-72a3-4551-92d6-7d6b8d979dd6': 'Security',
@@ -19,6 +24,36 @@ export class UserCardComponent implements OnInit {
     'b3a91e1e-2f42-4e3e-bf74-49b7c8aaf1c7': 'Finance'
     // Add more if needed
   };
+
+  showLicenseImage = false;
+
+  toggleLicenseImage(): void {
+    this.showLicenseImage = !this.showLicenseImage;
+  }
+  showFullImage = false;
+
+  openFullImage(): void {
+    this.showFullImage = true;
+  }
+
+  closeFullImage(): void {
+    this.showFullImage = false;
+  }
+
+
+
+  isImage(url: string | undefined | null): boolean {
+  if (!url) return false;
+  const imageExtensions = ['.png', '.jpg', '.jpeg'];
+  const ext = url.substring(url.lastIndexOf('.')).toLowerCase();
+  return imageExtensions.includes(ext);
+  }
+
+  getFullLicenseUrl(): string | null {
+    if (!this.user?.license_file_url) return null;
+    return `${environment.socketUrl}${this.user.license_file_url}`;
+  }
+
 
   constructor(
     private route: ActivatedRoute,
