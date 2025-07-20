c258607e7aefd5a05ab4627e299dfa8f5fa9a491
Karen7-salih <Karen7-salih.salihkaren345@gmail.com>
2025-07-20 08:11:48 +0300

    new model for feature

---
+++ Frontend/src/app/models/no-show-stats.model.ts
@@ -0,0 +1,13 @@
+export interface TopNoShowUser {
+  user_id: string;
+  name: string;
+  department_id: string;
+  count: number;
+}
+
+export interface NoShowStatsResponse {
+  total_no_show_events: number;
+  unique_no_show_users: number;
+  completed_rides_count: number;
+  top_no_show_users: TopNoShowUser[];
+}
