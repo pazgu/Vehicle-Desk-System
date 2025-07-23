2438ad443448e6463761e1a847a2b78ac7ae1c9f
pazgu <96666883+pazgu@users.noreply.github.com>
2025-07-23 12:25:26 +0300

    Update myrides.service.ts

--- Frontend/src/app/services/myrides.service.ts
+++ Frontend/src/app/services/myrides.service.ts
@@ -1,4 +1,4 @@
-coimport { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
