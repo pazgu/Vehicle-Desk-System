8dde08f42c9cb58220fce6939cdbaacb82e914d4
YareenHalaby <yareenh2006@gmail.com>
2025-09-04 09:06:15 +0300

    added submitted successfully card

--- Frontend/src/app/components/page-area/home/home.component.ts
+++ Frontend/src/app/components/page-area/home/home.component.ts
@@ -49,6 +49,7 @@ export class NewRideComponent implements OnInit {
     hours = Array.from({ length: 24 }, (_, i) => i.toString().padStart(2, '0'));
     quarterHours = ['00', '15', '30', '45'];
     formReady = false;
+    orderSubmitted = false;
 
     constructor(
         private fb: FormBuilder,
@@ -942,6 +943,45 @@ futureDateTimeValidator(): ValidatorFn {
 
     return null; // valid
   };
+  
+}
+    
+resetOrderForm(): void {
+  this.rideForm.reset();
+  this.step = 1;
+  this.orderSubmitted = false;
+  this.availableCars = [];
+  this.extraStops.clear();
+  this.showStep1Error = false;
+  this.disableRequest = false;
+  this.fetchedDistance = null;
+  this.estimated_distance_with_buffer = null;
+  
+  
+  this.rideForm.patchValue({
+    target_type: 'self',
+    target_employee_id: null,
+    ride_period: 'morning',
+    ride_date: '',
+    ride_date_night_end: '',
+    start_hour: '',
+    start_minute: '',
+    end_hour: '',
+    end_minute: '',
+    start_time: '',
+    end_time: '',
+    estimated_distance_km: null,
+    ride_type: '',
+    vehicle_type: '',
+    car: '',
+    stop: '',
+    four_by_four_reason: ''
+  });
+}
+
+openNewOrderForm(): void {
+  this.resetOrderForm();
+  this.initializeComponent(); 
 }
     submit(confirmedWarning = false): void {
         if (this.disableRequest) {
@@ -1036,13 +1076,14 @@ futureDateTimeValidator(): ValidatorFn {
             this.rideService.createRide(formData, user_id).subscribe({
             next: (createdRide) => {
                 this.toastService.show('הבקשה נשלחה בהצלחה! ✅', 'success');
+                this.orderSubmitted = true;
                 this.loadFuelType(formData.vehicle_id);
                 this.showFuelTypeMessage();
                 this.socketService.sendMessage('new_ride_request', {
                     ...createdRide,
                     user_id
                 });
-                this.router.navigate(['/']);
+                
             },
             error: (err) => {
                 const errorMessage = err.error?.detail || err.message || 'שגיאה לא ידועה';
@@ -1070,9 +1111,10 @@ futureDateTimeValidator(): ValidatorFn {
                      this.rideService.createSupervisorRide(formData, user_id).subscribe({
             next: () => {
                 this.toastService.show('הבקשה נשלחה בהצלחה! ✅', 'success');
+                this.orderSubmitted = true;
                 this.loadFuelType(formData.vehicle_id);
                 this.showFuelTypeMessage();
-                this.router.navigate(['/']);
+                
             },
             error: (err) => {
                 const errorMessage = err.error?.detail || err.message || 'שגיאה לא ידועה';
