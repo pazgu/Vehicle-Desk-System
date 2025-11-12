2e2fc919ae69c1e99bdad3d1c2e046dc9f1a9216
guev06 <jojamisto@gmail.com>
2025-11-12 08:06:06 +0200

    craeted ride-status component to split from admin analytics

---
+++ Frontend/src/app/components/admin-area/ride-status/ride-status.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ride-status',
+  imports: [],
+  templateUrl: './ride-status.component.html',
+  styleUrl: './ride-status.component.css'
+})
+export class RideStatusComponent {
+
+}
