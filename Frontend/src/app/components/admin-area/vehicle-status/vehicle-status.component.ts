410a16954f6dcad528d5442ad2755917d546bcc2
guev06 <jojamisto@gmail.com>
2025-11-12 09:30:37 +0200

    created ride status component for refactor

---
+++ Frontend/src/app/components/admin-area/vehicle-status/vehicle-status.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-status',
+  imports: [],
+  templateUrl: './vehicle-status.component.html',
+  styleUrl: './vehicle-status.component.css'
+})
+export class VehicleStatusComponent {
+
+}
