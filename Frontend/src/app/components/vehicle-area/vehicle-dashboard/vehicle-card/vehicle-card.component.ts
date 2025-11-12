18f7beb9b76da73a458eea106909fb7098fd0a89
leen <leen051206@gmail.com>
2025-11-12 11:46:00 +0200

    created 4 child components to be used in the vehicle-dashboard

---
+++ Frontend/src/app/components/vehicle-area/vehicle-dashboard/vehicle-card/vehicle-card.component.ts
@@ -0,0 +1,62 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { VehicleInItem } from '../../../../models/vehicle-dashboard-item/vehicle-in-use-item.module';
+import { VehicleUsageStatsComponent } from '../vehicle-usage-stats/vehicle-usage-stats.component';
+
+@Component({
+  selector: 'app-vehicle-card',
+  standalone: true,
+  imports: [CommonModule, CardModule, VehicleUsageStatsComponent],
+  templateUrl: './vehicle-card.component.html',
+  styleUrl: './vehicle-card.component.css',
+})
+export class VehicleCardComponent {
+  @Input() vehicle!: any;
+  @Input() showingMostUsed: boolean = false;
+  @Output() cardClick = new EventEmitter<string>();
+
+  @ViewChild(VehicleUsageStatsComponent)
+  usageStats!: VehicleUsageStatsComponent;
+
+  getCardClass(): string {
+    const statusClass = this.vehicle?.status
+      ? `card-${this.vehicle.status}`
+      : 'card-available';
+    return `vehicle-card ${statusClass}`; // will be applied to rendered .p-card element
+  }
+
+  isInactive(lastUsedAt: string | null | undefined): boolean {
+    if (!lastUsedAt) {
+      return true;
+    }
+
+    const lastUsedDate = new Date(lastUsedAt);
+    const sevenDaysAgo = new Date();
+    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+    return lastUsedDate < sevenDaysAgo;
+  }
+
+  onCardClick() {
+    this.cardClick.emit(this.vehicle?.id);
+  }
+
+  getVehicleUsageCount(plateNumber: string): number {
+    return this.usageStats?.getVehicleUsageCount(plateNumber) || 0;
+  }
+
+  getUsageLevel(plateNumber: string): 'high' | 'medium' | 'good' | 'hide' {
+    return this.usageStats?.getUsageLevel(plateNumber) || 'hide';
+  }
+
+  getUsageBarColor(plateNumber: string): string {
+    return this.usageStats?.getUsageBarColor(plateNumber) || '#E0E0E0';
+  }
+}
