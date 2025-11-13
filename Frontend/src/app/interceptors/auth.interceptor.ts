4a790d8e438d8e172fe134f2e9e1a149f675ad62
maysam-1 <maysamkh44@gmail.com>
2025-11-13 14:27:47 +0200

    deleted two tost when register

--- Frontend/src/app/interceptors/auth.interceptor.ts
+++ Frontend/src/app/interceptors/auth.interceptor.ts
@@ -45,9 +45,6 @@ export class AuthInterceptor implements HttpInterceptor {
           this.router.navigate(['/login']);
         } else if (err.status === 403) {
           this.toastService.show('אין לך הרשאות לגשת למשאב זה.', 'error'); // ✅ also convert this one
-        } else {
-            const msg = err.error?.detail || err.error?.message || `Request failed (${err.status})`;
-            this.toastService.show(msg, 'error');
         }
 
         return throwError(() => err);
