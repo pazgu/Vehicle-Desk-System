861001b1ca2ade9629882c38709689e1d9073110
yasminahalabi <yasminahalabi77@gmail.com>
2025-11-18 13:42:26 +0200

    added four_by_four_reason and extended_ride_reason in RideDetailsComponent

--- Frontend/src/app/ride-area/ride-details/ride-details.component.ts
+++ Frontend/src/app/ride-area/ride-details/ride-details.component.ts
@@ -57,9 +57,6 @@ export class RideDetailsComponent implements OnInit {
       this.toastService.show('שגיאה בטעינת פרטי הנסיעה', 'error');
     }
   });
-     // ✅ Socket: react to new ride request while on this page
-   
-  
   }
 canStartRide(): boolean {
   if (!this.ride) return false;
@@ -80,6 +77,25 @@ isRideLongerThanOneDay(): boolean {
   const endDay = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
   
   return endDay > startDay;
+}
+isExtendedRide(): boolean {
+  if (!this.ride?.start_datetime || !this.ride?.end_datetime) return false;
+  
+  const startDate = new Date(this.ride.start_datetime);
+  const endDate = new Date(this.ride.end_datetime);
+  
+  const diffInMs = endDate.getTime() - startDate.getTime();
+  const diffInDays = diffInMs / (1000 * 60 * 60 * 24) + 1;
+  
+  return diffInDays >= 4;
+}
+isSpecialVehicle(): boolean {
+  if (!this.ride?.vehicle_type) return false;
+  
+  const vehicleType = this.ride.vehicle_type.toLowerCase();
+  return vehicleType.includes('4x4') || 
+          vehicleType.includes('jeep') || 
+          vehicleType.includes('van');
 }
 
   startRide() {
