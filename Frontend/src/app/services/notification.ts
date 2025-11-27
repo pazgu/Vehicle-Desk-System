40b9304e4b867e3ce30563e8db7e56696630d3c1
sarahmanso <sarahmansor2580@gmail.com>
2025-11-27 14:36:09 +0200

    fixed with enviroemnts urls

--- Frontend/src/app/services/notification.ts
+++ Frontend/src/app/services/notification.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { MyNotification } from '../models/notification';
+import { environment } from '../../environments/environment';
 
 export interface AdminNotificationResponse {
   detail: string;
@@ -15,7 +16,7 @@ export interface AdminNotificationResponse {
   providedIn: 'root'
 })
 export class NotificationService {
-  private apiBase = 'http://127.0.0.1:8000/api';
+  private apiBase = environment.apiUrl;
   constructor(private http: HttpClient) {}
   public unreadCount$ = new BehaviorSubject<number>(0);
   getNotifications(): Observable<MyNotification[]> {
